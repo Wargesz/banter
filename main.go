c7cbebb6ccc6d41e1f1bb29635513678c941b9c9
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-10-07 01:00:00 +0200

    added main

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"banter/controllers"
+	"banter/initialisers"
+	"banter/middleware"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+    initialisers.LoadEnvVars()
+    initialisers.DbConnect()
+    initialisers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.tmpl")
+    routes(r)
+	r.Run(":" + os.Getenv("PORT"))
+}
+
+func routes(r *gin.Engine) {
+	r.GET("/", middleware.Auth, controllers.Posts)
+	r.GET("/login", controllers.LoginPage)
+	r.POST("/login", controllers.LoginUser)
+    r.GET("/signup", controllers.SignUpPage)
+    r.POST("/signup", controllers.SignUpUser)
+}
