8365769aed4f9aebd2ad5475e2ffed552ccd7dd4
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-10-20 00:16:04 +0200

    fixed abort on error

--- middleware/auth.go
+++ middleware/auth.go
@@ -11,36 +11,37 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-const SECRET = "myCoolTokenString"
-
 func Auth(c *gin.Context) {
 	tokenString, err := c.Cookie("Authorization")
 	if err != nil {
 		c.Redirect(http.StatusFound, "/login")
-		return
+        c.Abort()
 	}
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
 		return []byte(os.Getenv("SECRET")), nil
 	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
 	if err != nil {
 		c.Redirect(http.StatusFound, "/login")
+		c.Abort()
 	}
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
 		if float64(time.Now().Unix()) > claims["exp"].(float64) {
 			c.Redirect(http.StatusFound, "/login")
-			return
+			c.Abort()
 		}
-        var user models.User
-        initialisers.DB.First(&user, "id == ?", claims["sub"])
-        if user.ID == 0 {
+		var user models.User
+		result := initialisers.DB.First(&user, "username == ?", claims["sub"])
+		if result.Error != nil {
 			c.Redirect(http.StatusFound, "/login")
-			return
-        }
-        c.Set("user", user)
-        c.Next()
+			c.Abort()
+		}
+		if user.ID == 0 {
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+		}
+		c.Set("user", user)
         return
-	} else {
-		c.Redirect(http.StatusFound, "/login")
-		return
 	}
+	c.Redirect(http.StatusFound, "/login")
+    c.Abort()
 }
