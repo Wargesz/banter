33af67e7a921b739882a33242c08fbfc67fec6dc
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-10-20 01:40:54 +0200

    fixed early returns

--- middleware/auth.go
+++ middleware/auth.go
@@ -14,34 +14,38 @@ import (
 func Auth(c *gin.Context) {
 	tokenString, err := c.Cookie("Authorization")
 	if err != nil {
-		c.Redirect(http.StatusFound, "/login")
-        c.Abort()
+        abortWithRedirectToLogin(c)
+        return
 	}
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
 		return []byte(os.Getenv("SECRET")), nil
 	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
 	if err != nil {
-		c.Redirect(http.StatusFound, "/login")
-		c.Abort()
+		abortWithRedirectToLogin(c)
+        return
 	}
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
 		if float64(time.Now().Unix()) > claims["exp"].(float64) {
-			c.Redirect(http.StatusFound, "/login")
-			c.Abort()
+			abortWithRedirectToLogin(c)
+            return
 		}
 		var user models.User
 		result := initialisers.DB.First(&user, "username == ?", claims["sub"])
 		if result.Error != nil {
-			c.Redirect(http.StatusFound, "/login")
-			c.Abort()
+			abortWithRedirectToLogin(c)
+            return
 		}
 		if user.ID == 0 {
-			c.Redirect(http.StatusFound, "/login")
-			c.Abort()
+			abortWithRedirectToLogin(c)
+            return
 		}
 		c.Set("user", user)
-        return
+		return
 	}
-	c.Redirect(http.StatusFound, "/login")
-    c.Abort()
+    abortWithRedirectToLogin(c)
+}
+
+func abortWithRedirectToLogin(c *gin.Context) {
+    c.Redirect(http.StatusFound, "/login")
+	c.Abort()
 }
